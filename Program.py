d617afff25966490f8ce12963827104fbe9c29c8
Sancho2003 <saneksuper178@yandex.ru>
2020-10-06 22:48:06 +0300

    Fixing

--- Program.py
+++ Program.py
@@ -6,14 +6,17 @@ spaces = 0
 punctuation = 0
 words = 0
 sentences = 0
-with open ('steam_description_data.csv', encoding = 'utf-8') as f:
+with open('steam_description_data.csv', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
         line = ','.join(row)
         characters += len(line)
         spaces += line.count(' ')
-        punctuation += line.count('.') + line.count(',') + line.count('?') + line.count('!') + line.count('\'') + line.count('\"') + line.count(';') + line.count(':') + line.count('-') + line.count('(') + line.count(')')
-        words += len(line.split()) + len(line.split('.')) + len(line.split(',')) + len(line.split('?')) + len(line.split('!')) + len(line.split('\'')) + len(line.split('\"')) + len(line.split(';')) + len(line.split(':')) + len(line.split('-')) + len(line.split('(')) + len(line.split(')'))
+        punctuation += line.count('.') + line.count(',') + line.count('?') + line.count('!') + line.count(
+            '\'') + line.count('\"') + line.count(';') + line.count(':') + line.count('-') + line.count('(') + line.count(')')
+        words += len(line.split()) + len(line.split('.')) + len(line.split(',')) + len(line.split('?')) + len(line.split('!')) + len(line.split('\'')) + \
+            len(line.split('\"')) + len(line.split(';')) + len(line.split(':')) + \
+            len(line.split('-')) + len(line.split('(')) + len(line.split(')'))
         sentences += len(re.findall(r"([A-Z][^\.!?]*[\.!?])", line))
 print('Общее количество символов:', characters)
 print('Количество символов без пробелов:', characters - spaces)
